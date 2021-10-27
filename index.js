609023a4148c1b8b187d272b8992179b18f929d2
Medul Hasan <medul.cse@gmail.com>
2021-10-28 00:42:53 +0600

    get event data for client

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6hb5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('volunteer_network');
+        const eventCollection = database.collection('events');
+
+        app.get('/events', async (req, res) => {
+            const events = eventCollection.find({});
+            const result = await events.toArray();
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Volunteer Network');
+})
+
+app.listen(port, () => {
+    console.log('Server is Running on port ', port);
+})
